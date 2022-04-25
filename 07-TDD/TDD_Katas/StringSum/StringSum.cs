056e962f8b4ed85c344ec6024161f54fc58214be
Anton Yermolayev <Anton_Yermolayev@epam.com>
2022-04-25 23:47:10 +0600

    Homework07 (TDD StringSum) Initial commit

---
+++ 07-TDD/TDD_Katas/StringSum/StringSum.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace StringSum
+{
+    public class StringSum
+    {
+    }
+}
---
+++ 07-TDD/TDD_Katas/StringSumTests/Tests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace StringSum
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
