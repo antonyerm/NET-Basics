a0847afe3f9e9e096595ab6d90cee20d5dd9d009
Anton Yermolayev <Anton_Yermolayev@epam.com>
2022-04-27 11:58:49 +0600

    Homework07 (TDD LeapYear) Add test with various leap and common years

--- 07-TDD/TDD_Katas/LeapYearTests/Tests.cs
+++ 07-TDD/TDD_Katas/LeapYearTests/Tests.cs
@@ -43,5 +43,22 @@ namespace LeapYearKata
             //assert
             Assert.IsTrue(actual);
         }
+
+        [Test]
+        [TestCase(2001, false)]
+        [TestCase(1996, true)]
+        [TestCase(1900, false)]
+        [TestCase(2000, true)]
+        public void LeapYear_VariousLeapAndCommonYears_ReturnsRespectively(int number, bool expected)
+        {
+            //arrange
+            var yearChecker = new LeapYear();
+
+            //act
+            var actual = yearChecker.IsLeapYear(number);
+
+            //assert
+            Assert.That (actual == expected);
+        }
     }
 }
