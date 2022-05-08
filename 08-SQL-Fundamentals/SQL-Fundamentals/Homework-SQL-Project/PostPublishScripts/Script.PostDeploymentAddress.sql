1d77d65d47b52768459e10f1abbe17b196ac79a8
Anton Yermolayev <Anton_Yermolayev@epam.com>
2022-05-09 01:23:01 +0600

    Homework08 (SQL-Fundamentals) Add PostDeployment scripts for initial filling of the database

---
+++ 08-SQL-Fundamentals/SQL-Fundamentals/Homework-SQL-Project/PostPublishScripts/Script.PostDeploymentAddress.sql
@@ -0,0 +1,20 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+IF NOT EXISTS (SELECT 1 FROM [Address])
+BEGIN
+    INSERT INTO [Address] ([Street], [City], [State], [ZipCode])
+    VALUES ('West Main','Liverpool','-','04030'),
+           ('Abbey Road','London','-','594590'),
+           ('East Lake','New York','NY','343566'),
+           ('Hill','Denver','CO','54567'),
+           ('Oak','Boston','MA','35678')
+END
