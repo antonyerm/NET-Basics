e6ab4afa100fe49be99ec98953d5aa8b67df8837
Anton Yermolayev <Anton_Yermolayev@epam.com>
2022-05-27 11:49:27 +0600

    Homework11 (OOP) Implement FileCabinet app (tasks 1 and 2)

---
+++ 11-FileCabinet/FileCabinet/Cabinet.cs
@@ -0,0 +1,32 @@
+﻿using FileCabinet.Interfaces;
+using FileCabinet.Output;
+using FileCabinet.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileCabinet
+{
+    class Cabinet
+    {
+        private IStorage _storage;
+        private IOutput _output;
+
+        public Cabinet(IStorage storage = null, IOutput output = null)
+        {
+            _storage = storage == null ? new FileStorage() : storage;
+            _output = output == null ? new ConsoleOutput() : output;
+        }
+
+        public IDocument SearchDocumentByNumber(int number)
+        {
+            var document = _storage.GetDocumentByNumber(number);
+            if (document is IPrintable printableDocument)
+            {
+                printableDocument.PrintDocumentProperties(_output);
+            }
+
+            return document;
+        }
+    }
+}
