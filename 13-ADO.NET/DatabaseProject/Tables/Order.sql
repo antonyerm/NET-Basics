da893bd1d54d325116577cbccacdd9dbe6a61ec3
Anton Yermolayev <Anton_Yermolayev@epam.com>
2022-06-23 22:39:10 +0600

    Homework13 (ADO.NET) Create database.

---
+++ 13-ADO.NET/DatabaseProject/Tables/Order.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Order]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Status] NCHAR(15) NULL, 
+    [CreatedDate] DATETIME NULL, 
+    [UpdatedDate] DATETIME NULL, 
+    [ProductId] INT NULL, 
+    CONSTRAINT [FK_Order_Product] FOREIGN KEY ([ProductId]) REFERENCES [Product]([Id]) 
+)
